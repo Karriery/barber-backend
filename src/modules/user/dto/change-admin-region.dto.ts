10849b84c3b764ce0350e3d68b6b32e6d229bb70
samybchaalia <sami.benchaalia@sesame.com.tn>
2022-09-22 11:14:27 +0100

    user , admin and auth modules

--- src/modules/user/dto/change-admin-region.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsNumber, IsNotEmpty } from 'class-validator';
-
-export class ChangeAdminRegion {
-    @IsNumber()
-    @IsNotEmpty()
-    @ApiProperty()
-    regionId: number;
-}
--- src/modules/user/dto/update-speciality.input.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { CreateSpecialityDto } from "./create-speciality.input.dto copy";
-
-export class UpdateSpecialityDto extends PartialType(CreateSpecialityDto) {}
